c84abd5945ec56233a0bcbd4200e9506bf669d0e
Sota2501 <soutan2501@icloud.com>
2022-10-16 17:35:06 +0900

    ChatTickerScrollのボタン非表示設定の追加・実装

--- content.js
+++ content.js
@@ -568,7 +568,12 @@ class FullscreenChat extends Ext {
 class ChatTickerScroll extends Ext {
 	static name = "ChatTickerScroll";
 	static description = this.i18n("Description");
-	static ticker = document.querySelector("#ticker yt-live-chat-ticker-renderer");
+	static style = `
+		#ticker :is(#left-arrow-container, #right-arrow-container) {
+			display: none;
+		}
+	`;
+	static ticker;
 	static buttons = {
 		true: document.querySelector("#ticker #left-arrow-container yt-icon"),
 		false: document.querySelector("#ticker #right-arrow-container yt-icon")
@@ -577,15 +582,37 @@ class ChatTickerScroll extends Ext {
 		true: null,
 		false: null
 	};
+	static registOptions(wrapper){
+		(new DOMTemplate(wrapper))
+			.ins("append","caption",{
+				captionInput: "toggle",
+				captionDescription: this.i18n("ButtonHide"),
+				toggleOptionName: `${this.name}-opt-button-hide`,
+				toggleChecked: (Storage.getOption(`${this.name}-opt-button-hide`,true)?" checked":"")
+			},true)
+			.on({q:"#ext-yc-toggle",t:"click",f:Options.toggle})
+	}
+	static optionsUpdated(opts){
+		if(opts["opt-button-hide"]){
+			this.setStyle(this.style);
+		}else{
+			this.removeStyle();
+		}
+	}
 	static init(){
 		if(YoutubeState.isChatFrame()){
 			YoutubeEvent.addEventListener("load",()=>{
+				if(Storage.getOption(`${this.name}-opt-button-hide`,true)){
+					this.setStyle(this.style);
+				}
+				this.ticker = document.querySelector("#ticker yt-live-chat-ticker-renderer");
 				this.ticker.addEventListener("wheel",this.scrollTicker);
 			});
 		}
 	}
 	static deinit(){
 		if(YoutubeState.isChatFrame()){
+			this.removeStyle();
 			this.ticker.removeEventListener("wheel",this.scrollTicker);
 		}
 	}
