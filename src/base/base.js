7b14c323f28d660711b069726c9305cc0d04fafa
Sota2501 <soutan2501@icloud.com>
2024-10-22 22:03:14 +0900

    JSDoc追加

--- src/base/base.js
+++ src/base/base.js
@@ -1,8 +1,21 @@
+/**
+ * @typedef {import('../core/event_manager').default} EventManager
+ * @typedef {import('../core/status_manager').default} StatusManager
+ * @typedef {import('../core/debug').default} Debug
+ */
+
 export default class Base {
 	register(event, status, debug) {
+		/** @type EventManager */
 		this._event = event;
+
+		/** @type StatusManager */
 		this._status = status;
+
+		/** @type Debug */
 		this._debug = debug;
+
+		/** @type this */
 		this._bind = new Proxy({}, {
 			get: (target, prop) => {
 				if (prop in target) {
